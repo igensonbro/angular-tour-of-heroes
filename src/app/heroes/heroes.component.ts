106a72c9efd4518a56a894d31e671773c7217211
Azriel Abramovich <aabramovich@gmail.com>
2018-09-11 11:50:42 +0100

    Completed Servicesw

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -16,7 +17,7 @@ export class HeroesComponent implements OnInit {
     name : 'Windstorm'
   };
 
-  heroes = HEROES;
+  heroes : Hero[];
 
   selectedHero: Hero;
 
@@ -24,9 +25,14 @@ export class HeroesComponent implements OnInit {
     this.selectedHero = hero;
   }
 
-  constructor() { }
+  getHeroes() : void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+
+  constructor(private heroService : HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
 }
